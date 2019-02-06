5332678fb87e57766306e8b7a079243ba4f4baed
behrmann97 <sbehrmann@uc.cl>
2019-02-06 16:22:47 -0300

    base files

---
+++ clases.py
@@ -0,0 +1,104 @@
+from PyQt5.QtGui import QPixmap, QTransform, QCursor, QIcon, QImage, QBrush, \
+    QPalette, QFont
+from PyQt5.QtCore import QTimer, pyqtSignal, QObject, QSize, Qt, QThread, \
+    QCoreApplication
+from PyQt5.QtWidgets import QLabel, QWidget, QMainWindow, QApplication, \
+    QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit, QProgressBar, QGroupBox
+from PyQt5.Qt import QTest, QTransform, QSound
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+import time
+
+
+class TextEdit(QLineEdit):
+
+    def __init__(self, parent = None):
+        super().__init__(parent)
+
+        self.setText('ej: 12345678-9')
+        self.numbers = ''
+        self.digit = ''
+
+
+    def mousePressEvent(self, event):
+        super().mousePressEvent(event)
+        self.parent().keyboard.show()
+        self.parent().error_label.hide()
+        if self.text() =='ej: 12345678-9':
+             self.setText('')
+
+
+    def keyPress(self, key):
+        self.parent().error_label.hide()
+        self.parent().start = time.time()
+
+        if key == 'DEL':
+            if self.numbers:
+                end = self.numbers[len(self.numbers)-1]
+                self.numbers = self.numbers[0:len(self.numbers) -1]
+                self.digit = end
+            else:
+                self.digit = ''
+
+        else:
+            self.numbers += self.digit
+            self.digit = key
+
+
+        self.setText(self.numbers + '-' + self.digit)
+
+    def isValidRut(self):
+        if len(self.numbers) < 7 or len(self.numbers) > 8:
+            return False
+        if 'K' in self.numbers:
+            return False
+        multipliers = [2,3,4,5,6,7]
+        counter = 0
+        total = 0
+        for i in self.numbers[::-1]:
+            total += int(i) * multipliers[counter%6]
+            counter += 1
+        if self.digit == 'K' and str(11- total%11) == 10:
+            return True
+        if str(11 -(total%11 if total%11 !=0 else  11 )) == self.digit:
+            return True
+        return False
+
+    def resetRut(self):
+        self.numbers = ''
+        self.digit = ''
+        self.setText('ej: 12345678-9')
+
+
+class Button(QPushButton):
+
+    def __init__(self, parent, key, icon= None):
+        super().__init__(parent)
+        self.clicked.connect(lambda: self.parent().editor.keyPress(key))
+        if icon:
+            self.setIcon(QIcon(icon))
+            self.setIconSize(QSize(30,30))
+        else:
+            self.setText(key)
+
+
+
+class Keyboard(QWidget):
+
+    def __init__(self, parent, editor):
+        super().__init__(parent)
+        self.editor = editor
+        self.buttons = []
+
+        for i in range(10):
+            button = Button(self, str((i+1)%10))
+            self.buttons.append(button)
+            button.setGeometry(((i)%3* 70) + i//9 * 70, i//3 *70, 70, 70)
+            button.show()
+        button = Button(self, 'K')
+        self.buttons.append(button)
+        button.setGeometry(0,210, 70, 70)
+        button.show()
+        button = Button(self,'DEL','assets/delete')
+        self.buttons.append(button)
+        button.setGeometry(140, 210, 70, 70)
+        button.show()
---
+++ main.py
@@ -0,0 +1,255 @@
+from PyQt5.QtGui import QPixmap, QTransform, QCursor, QIcon, QImage, QBrush, \
+    QPalette, QFont
+from PyQt5.QtCore import QTimer, pyqtSignal, QObject, QSize, Qt, QThread, \
+    QCoreApplication
+from PyQt5.QtWidgets import QLabel, QWidget, QMainWindow, QApplication, \
+    QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit, QProgressBar, QGroupBox
+from PyQt5.Qt import QTest, QTransform, QSound
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from clases import TextEdit, Keyboard
+import json
+import time
+
+class Window(QWidget):
+
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+        self.show()
+        self.start = time.time()
+        self.small = True
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.check_time)
+        self.smallScreen()
+
+        self.background = QLabel(self)
+        self.background.setGeometry(0,0, SCREEN_WIDTH,SCREEN_HEIGHT)
+        self.background.setStyleSheet("background-color: rgba(0,0,255,50%)")
+
+        self.newfont = QFont("Times", 30)
+        self.smallfont = QFont("Times", 15)
+        self.errorfont = QFont("Times", 10)
+
+        self.icon = QLabel(self)
+        self.icon.setGeometry(SCREEN_WIDTH/2 - 200, SCREEN_HEIGHT/4 - 48, 400, 96)
+        self.icon.setPixmap(QPixmap('assets/logo').scaled(400,96))
+
+    def check_time(self):
+        newtime = time.time()
+        if not self.small and newtime - self.start >= 5:
+            self.smallScreen()
+
+
+    def smallScreen(self):
+        if not self.small:
+            self.charge_button.hide()
+            self.take_button.hide()
+            self.cancel_button.hide()
+            self.rut_editor.hide()
+            self.enter_button.hide()
+            self.error_label.hide()
+            self.keyboard.hide()
+            self.box_button.hide()
+            self.box_text.hide()
+            self.text.hide()
+        self.timer.stop()
+        self.small = True
+        self.setGeometry(SCREEN_WIDTH-100, SCREEN_HEIGHT -100, 95, 95)
+        self.small_icon = QLabel(self)
+        self.small_icon.setPixmap(QPixmap('assets/logo_small').scaled(95,95))
+        self.small_icon.show()
+
+    def bigScreen(self):
+        self.small_icon.hide()
+        self.start = time.time()
+        self.small = False
+        self.setGeometry(0,0, SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.timer.start(500)
+        with open('data.txt') as json_file:
+            self.boxes = json.load(json_file)
+        self.show()
+        self.background.show()
+        self.icon.show()
+
+        self.charge_button = QPushButton('Cargar celular', self)
+        self.charge_button.setGeometry(SCREEN_WIDTH/2 - 200, SCREEN_HEIGHT/4 + 150, 200, 70)
+        self.charge_button.clicked.connect(self.charge)
+        self.charge_button.setFont(self.smallfont)
+
+        self.take_button = QPushButton('Retirar celular', self)
+        self.take_button.setGeometry(SCREEN_WIDTH/2 + 20, SCREEN_HEIGHT/4 + 150, 200, 70)
+        self.take_button.clicked.connect(self.take)
+        self.take_button.setFont(self.smallfont)
+
+        self.text = QLabel('Ingresa tu RUT :', self)
+        self.text.setGeometry(SCREEN_WIDTH/2 - 400, SCREEN_HEIGHT/4 + 150, 400, 60)
+        self.text.setFont(self.newfont)
+
+        self.rut_editor = TextEdit(self)
+        self.rut_editor.setGeometry(SCREEN_WIDTH/2 + 50, SCREEN_HEIGHT/4 + 150, 200, 60)
+
+        self.enter_button = QPushButton('Ingresar', self)
+        self.enter_button.setGeometry(SCREEN_WIDTH/2 + 300, SCREEN_HEIGHT/4 + 150, 100, 60)
+        self.enter_button.clicked.connect(self.EnterRut)
+
+        self.error_label = QLabel('RUT inválido', self)
+        self.error_label.setGeometry(SCREEN_WIDTH/2+250, SCREEN_HEIGHT/4 + 220, 200, 60)
+        self.error_label.setFont(self.errorfont)
+        self.error_label.setStyleSheet('color:red')
+
+        self.keyboard = Keyboard(self, self.rut_editor)
+        self.keyboard.setGeometry(SCREEN_WIDTH/2 -75, SCREEN_HEIGHT/4 + 250, 210, 280)
+
+
+        self.box_text = QLabel(self)
+        self.box_text.setGeometry(SCREEN_WIDTH/2 - 200, SCREEN_HEIGHT/4 + 100, 400, 200)
+        self.box_text.setFont(self.smallfont)
+
+        self.box_button = QPushButton('Cerrar casilla', self)
+        self.box_button.setFont(self.smallfont)
+        self.box_button.setGeometry(SCREEN_WIDTH/2 - 230, SCREEN_HEIGHT/4 + 320, 160, 50 )
+        self.box_button.clicked.connect(self.closeBox)
+
+        self.cancel_button = QPushButton('Cancelar',self)
+        self.cancel_button.setFont(self.smallfont)
+        self.cancel_button.setGeometry(SCREEN_WIDTH/2 +180, SCREEN_HEIGHT/4 + 320, 160, 50)
+        self.cancel_button.clicked.connect(self.setUp)
+
+        self.setUp()
+
+    def setUp(self):
+
+        self.rut = ''
+        self.rut_editor.resetRut()
+
+        self.taking = False
+        self.charge_button.show()
+        self.take_button.show()
+
+        self.text.hide()
+        self.rut_editor.hide()
+        self.enter_button.hide()
+
+        self.box_text.hide()
+        self.box_button.hide()
+        self.cancel_button.hide()
+        self.keyboard.hide()
+
+
+    def charge(self):
+        self.start = time.time()
+        self.box_text.hide()
+        self.cancel_button.show()
+        self.taking = False
+        if not self.getBox():
+            self.box_text.show()
+            self.box_text.setText('''Lo sentimos, en este
+momento no hay casillas disponibles''')
+        else:
+            self.text.show()
+            self.rut_editor.show()
+            self.enter_button.show()
+
+        self.charge_button.hide()
+        self.take_button.hide()
+        self.box_button.hide()
+
+    def take(self):
+        self.start = time.time()
+        self.taking = True
+        self.text.show()
+        self.rut_editor.show()
+        self.enter_button.show()
+        self.cancel_button.show()
+
+        self.charge_button.hide()
+        self.take_button.hide()
+
+        self.box_text.hide()
+        self.box_button.hide()
+
+    def mousePressEvent(self, event):
+        self.start = time.time()
+        if self.small:
+            self.hide()
+            self.bigScreen()
+        else:
+            self.keyboard.hide()
+
+    def EnterRut(self):
+        self.start = time.time()
+        if self.rut_editor.isValidRut():
+            self.keyboard.hide()
+            self.enter_button.hide()
+            self.text.hide()
+            self.rut_editor.hide()
+            self.rut = self.rut_editor.numbers
+
+            if self.taking:
+                box = self.openBox()
+                self.box_text.show()
+                if box:
+                    self.box_text.setText('''Por favor, retira tu celular
+    en la casilla número {}'''.format(box))
+                else:
+                    self.box_text.setText('''No hay ninguna casilla
+                    asignada a ese RUT''')
+                self.cancel_button.show()
+
+            else:
+                box = self.getBox()
+                self.box_text.show()
+                if box != 'MAX':
+                    self.box_text.setText('''Por favor, deja tu celular
+    en la casilla número {}'''.format(box))
+
+                    self.box_button.show()
+                else:
+                    self.box_text.setText('''Ya hay una casilla asignada a
+    este RUT''')
+
+                self.cancel_button.show()
+
+        else:
+            self.error_label.show()
+
+
+
+
+    def getBox(self):
+        for i in self.boxes:
+            if self.boxes[i] == self.rut:
+                return 'MAX'
+        for i in self.boxes:
+            if not self.boxes[i]:
+                return i
+        return False
+
+
+    def closeBox(self):
+        self.start = time.time()
+        self.boxes[self.getBox()] = self.rut
+        with open('data.txt', 'w') as file:
+            json.dump(self.boxes, file)
+        self.setUp()
+
+    def openBox(self):
+        self.start = time.time()
+        for i in self.boxes:
+            if self.boxes[i] == self.rut:
+                self.boxes[i] = False
+                with open('data.txt', 'w') as file:
+                    json.dump(self.boxes, file)
+                return i
+        return False
+
+
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    editor = Window()
+    app.exec_()
